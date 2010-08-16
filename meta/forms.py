c20a56b6aa5408fb38a732831f7ccf54105bdc63
nelas <none@none>
2010-08-16 19:11:05 -0300

    Corrigi bug do buscador com busca vazia.

--- meta/forms.py
+++ meta/forms.py
@@ -5,5 +5,5 @@ class SearchForm(forms.Form):
     query = forms.CharField(
             label=u'Buscar por',
             widget=forms.TextInput(attrs={'size': 32}),
-            #help_text=u'Digite um ou mais termos.',
+            help_text=u'(digite um ou mais termos)',
             )

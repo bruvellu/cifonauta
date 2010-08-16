d8dc50b01b5633d99000c1909180f3ba04e357f3
nelas <none@none>
2010-08-16 18:29:19 -0300

    Adicionei total de ítens encontrados e mudei formulário de busca.

--- meta/forms.py
+++ meta/forms.py
@@ -3,7 +3,7 @@ from django import forms
     
 class SearchForm(forms.Form):
     query = forms.CharField(
-            label=u'Buscar:',
+            label=u'Buscar por',
             widget=forms.TextInput(attrs={'size': 32}),
             #help_text=u'Digite um ou mais termos.',
             )

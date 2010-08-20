6cd790a6ded603320d52dd90f7c6cb2ecb302bcd
nelas <none@none>
2010-08-20 17:19:37 -0300

    Substituí a tag de vídeo na página principal.
    Preciso dar um jeito nesse tocador de vídeo...

--- meta/models.py
+++ meta/models.py
@@ -58,6 +58,7 @@ class Image(File):
 class Video(File):
     web_filepath = models.FileField(upload_to='site_media/videos/')
     datatype = models.CharField(max_length=10, default='video')
+    large_thumb = models.ImageField(upload_to='site_media/images/thumbs')
 
     def __unicode__(self):
         return self.title

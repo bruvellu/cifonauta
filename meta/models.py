a8a84e589da50e56a0dfb0b0e741d6efb032966c
Bruno Vellutini <organelas@gmail.com>
2010-11-30 22:25:36 -0200

    Arrumei ordem dos módulos e arrumei referências/Mendeley.

--- meta/models.py
+++ meta/models.py
@@ -4,7 +4,7 @@ from django.db.models import signals
 from django.db.models import permalink
 from django.template.defaultfilters import slugify
 
-from mendeley import mendeley
+from external.mendeley import mendeley
 
 
 class File(models.Model):

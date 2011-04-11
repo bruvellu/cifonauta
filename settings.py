feff083668e86acc56e41b5c814b4cfa259e6646
Bruno Vellutini <organelas@gmail.com>
2011-04-11 19:28:37 -0300

    Acertei o estilo das citações. fixes #78

--- settings.py
+++ settings.py
@@ -44,6 +44,7 @@ SITE_ID = 1
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
+USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"

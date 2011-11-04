cdc790b7f233c62252dd8f6bd0b705e6edcc274e
Bruno Vellutini <organelas@gmail.com>
2011-11-03 22:46:30 -0200

    Mostra mensagem indicando que tour não-público está indisponível.

--- meta/views.py
+++ meta/views.py
@@ -729,6 +729,7 @@ def meta_page(request, model_name, field, slug):
 
 def tour_page(request, slug):
     '''Página única de cada tour.'''
+    #TODO mostra mensagem em template de tour não público.
     tour = get_object_or_404(Tour, slug=slug)
     references = tour.references.all()
     photos = tour.images.filter(tourposition__tour=tour).select_related('size', 'sublocation', 'city', 'state', 'country').order_by('tourposition')

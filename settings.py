5cc36957706df261f420cd316e06a63dc7c3a3d2
Bruno Vellutini <organelas@gmail.com>
2011-11-29 01:30:14 +0100

    Quebra-galho para Dajaxice (e contador) voltar a funcionar

--- settings.py
+++ settings.py
@@ -96,11 +96,13 @@ SECRET_KEY = ''
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-        ('django.template.loaders.cached.Loader', (
-            'django.template.loaders.app_directories.Loader',
-            #'django.template.loaders.filesystem.Loader',
-            )),
-        #'django.template.loaders.eggs.Loader',
+        #('django.template.loaders.cached.Loader', (
+        #    'django.template.loaders.app_directories.Loader',
+        #    #'django.template.loaders.filesystem.Loader',
+        #    )),
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+        'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (

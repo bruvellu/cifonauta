42b0b1b0119161230dbb3c6da29a6c5887076307
Bruno Vellutini <organelas@gmail.com>
2015-03-01 01:17:00 +0100

    Refactor translate ranks command

--- meta/management/commands/translate_ranks.py
+++ meta/management/commands/translate_ranks.py
@@ -15,22 +15,57 @@ class Command(BaseCommand):
             self.stdout.write('%s id=%d' % (taxon.name, taxon.id))
             if taxon.rank_pt_br and taxon.rank_en:
                 continue
-            elif taxon.rank_pt_br and not taxon.rank_en:
-                taxon.rank_en = translate_pt2en(taxon.rank_pt_br)
+            elif not taxon.rank_pt_br or not taxon.rank_en:
+                taxon.rank_en = translate_rank(taxon.rank_pt_br)
+                taxon.rank_pt_br = translate_rank(taxon.rank_en)
                 taxon.save()
-                self.stdout.write('Translated %s -> %s!' % (taxon.rank_pt_br, taxon.rank_en))
-            elif not taxon.rank_pt_br and taxon.rank_en:
-                taxon.rank_pt_br = translate_en2pt(taxon.rank_en)
-                taxon.save()
-                self.stdout.write('Translated %s -> %s!' % (taxon.rank_en, taxon.rank_pt_br))
             else:
                 continue
 
         self.stdout.write('Finished translation.')
 
 
-def translate_en2pt(rank):
-    '''Translate rank to portuguese.'''
+def translate_rank(rank):
+    '''Translate ranks inteligently.'''
+
+    pt2en = {
+            u'Subforma': u'Subform',
+            u'Superordem': u'Superorder',
+            u'Variedade': u'Variety',
+            u'Infraordem': u'Infraorder',
+            u'Seção': u'Section',
+            u'Subclasse': u'Subclass',
+            u'Subseção': u'Subsection',
+            u'Reino': u'Kingdom',
+            u'Infrareino': u'Infrakingdom',
+            u'Divisão': u'Division',
+            u'Subtribo': u'Subtribe',
+            u'Aberração': u'Aberration',
+            u'Infraordem': u'InfraOrder',
+            u'Subreino': u'Subkingdom',
+            u'Infraclasse': u'Infraclass',
+            u'Subfamília': u'Subfamily',
+            u'Classe': u'Class',
+            u'Superfamília': u'Superfamily',
+            u'Subdivisão': u'Subdivision',
+            u'Morfotipo': u'Morph',
+            u'Raça': u'Race',
+            u'Não especificado': u'Unspecified',
+            u'Subordem': u'Suborder',
+            u'Gênero': u'Genus',
+            u'Ordem': u'Order',
+            u'Subvariedade': u'Subvariety',
+            u'Tribo': u'Tribe',
+            u'Subgênero': u'Subgenus',
+            u'Forma': u'Form',
+            u'Família': u'Family',
+            u'Subfilo': u'Subphylum',
+            u'Estirpe': u'Stirp',
+            u'Filo': u'Phylum',
+            u'Superclasse': u'Superclass',
+            u'Subespécie': u'Subspecies',
+            u'Espécie': u'Species',
+            }
 
     en2pt = {
             u'Subform': u'Subforma',
@@ -72,59 +107,14 @@ def translate_en2pt(rank):
             }
 
     try:
-        rank_pt = en2pt[rank]
-        return rank_pt
+        rank_translation = en2pt[rank]
+        return rank_translation
     except:
-        rank_pt = translate_pt2en(rank)
-        return rank_pt
-
-
-def translate_pt2en(rank):
-    '''Translate rank to english.'''
-
-    pt2en = {
-            u'Subforma': u'Subform',
-            u'Superordem': u'Superorder',
-            u'Variedade': u'Variety',
-            u'Infraordem': u'Infraorder',
-            u'Seção': u'Section',
-            u'Subclasse': u'Subclass',
-            u'Subseção': u'Subsection',
-            u'Reino': u'Kingdom',
-            u'Infrareino': u'Infrakingdom',
-            u'Divisão': u'Division',
-            u'Subtribo': u'Subtribe',
-            u'Aberração': u'Aberration',
-            u'Infraordem': u'InfraOrder',
-            u'Subreino': u'Subkingdom',
-            u'Infraclasse': u'Infraclass',
-            u'Subfamília': u'Subfamily',
-            u'Classe': u'Class',
-            u'Superfamília': u'Superfamily',
-            u'Subdivisão': u'Subdivision',
-            u'Morfotipo': u'Morph',
-            u'Raça': u'Race',
-            u'Não especificado': u'Unspecified',
-            u'Subordem': u'Suborder',
-            u'Gênero': u'Genus',
-            u'Ordem': u'Order',
-            u'Subvariedade': u'Subvariety',
-            u'Tribo': u'Tribe',
-            u'Subgênero': u'Subgenus',
-            u'Forma': u'Form',
-            u'Família': u'Family',
-            u'Subfilo': u'Subphylum',
-            u'Estirpe': u'Stirp',
-            u'Filo': u'Phylum',
-            u'Superclasse': u'Superclass',
-            u'Subespécie': u'Subspecies',
-            u'Espécie': u'Species',
-            }
-
-    try:
-        rank_en = pt2en[rank]
-        return rank_en
-    except:
-        rank_en = translate_en2pt(rank)
-        return rank_en
+        try:
+            rank_translation = pt2en[rank]
+            return rank_translation
+        except:
+            return rank
+    else:
+        return rank
 

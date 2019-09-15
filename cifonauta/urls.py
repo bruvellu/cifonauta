782b7d7e2d54be1cb32191bde1459ad99e0d97c9
Bruno Vellutini <organelas@gmail.com>
2019-09-15 14:47:47 +0200

    Merges from develop branch

--- cifonauta/urls.py
+++ cifonauta/urls.py
@@ -7,11 +7,6 @@ from django.urls import include, path
 
 #admin.autodiscover()
 
-#from django.views.decorators.cache import cache_page
-#from django.contrib.sitemaps import FlatPageSitemap, GenericSitemap
-#from django import template
-#template.add_to_builtins('meta.templatetags.extra_tags')
-
 #ONE_HOUR = 60 * 60                  # 3600
 #HALF_DAY = 60 * 60 * 12             # 43200
 #ONE_DAY = 60 * 60 * 24              # 86400
@@ -22,39 +17,36 @@ from django.urls import include, path
 #ONE_YEAR = 60 * 60 * 24 * 30 * 12   # 31104000
 
 # Sitemaps
-#photo_dict = {'queryset': Image.objects.filter(is_public=True), 'date_field': 'timestamp'}
-#video_dict = {'queryset': Video.objects.filter(is_public=True), 'date_field': 'timestamp'}
-#tour_dict = {'queryset': Tour.objects.filter(is_public=True), 'date_field': 'timestamp'}
-#taxon_dict = {'queryset': Taxon.objects.all()}
-#tag_dict = {'queryset': Tag.objects.all()}
-#place_dict = {'queryset': Sublocation.objects.all()}
-#city_dict = {'queryset': City.objects.all()}
-#state_dict = {'queryset': State.objects.all()}
-#country_dict = {'queryset': Country.objects.all()}
-#author_dict = {'queryset': Author.objects.all()}
-#source_dict = {'queryset': Source.objects.all()}
-#reference_dict = {'queryset': Reference.objects.all()}
+# photo_dict = {'queryset': Image.objects.filter(is_public=True), 'date_field': 'timestamp'}
+# video_dict = {'queryset': Video.objects.filter(is_public=True), 'date_field': 'timestamp'}
+# tour_dict = {'queryset': Tour.objects.filter(is_public=True), 'date_field': 'timestamp'}
+# taxon_dict = {'queryset': Taxon.objects.all()}
+# tag_dict = {'queryset': Tag.objects.all()}
+# place_dict = {'queryset': Sublocation.objects.all()}
+# city_dict = {'queryset': City.objects.all()}
+# state_dict = {'queryset': State.objects.all()}
+# country_dict = {'queryset': Country.objects.all()}
+# author_dict = {'queryset': Author.objects.all()}
+# source_dict = {'queryset': Source.objects.all()}
+# reference_dict = {'queryset': Reference.objects.all()}
 
-#sitemaps = {
-#    'flatpages': FlatPageSitemap,
-#    #'blog': GenericSitemap(blog_dict, priority=0.4, changefreq='monthly'),
-#    'photos': GenericSitemap(photo_dict, priority=0.7, changefreq='weekly'),
-#    'videos': GenericSitemap(video_dict, priority=0.7, changefreq='weekly'),
-#    'tours': GenericSitemap(tour_dict, priority=0.8, changefreq='monthly'),
-#    'taxa': GenericSitemap(taxon_dict, priority=1.0, changefreq='weekly'),
-#    'tags': GenericSitemap(tag_dict, priority=0.8, changefreq='weekly'),
-#    'places': GenericSitemap(place_dict, priority=0.8, changefreq='weekly'),
-#    'cities': GenericSitemap(city_dict, priority=0.6, changefreq='monthly'),
-#    'states': GenericSitemap(state_dict, priority=0.4, changefreq='monthly'),
-#    'countries': GenericSitemap(country_dict, priority=0.4, changefreq='monthly'),
-#    'authors': GenericSitemap(author_dict, priority=0.8, changefreq='weekly'),
-#    'sources': GenericSitemap(source_dict, priority=0.7, changefreq='weekly'),
-#    'references': GenericSitemap(reference_dict, priority=0.5, changefreq='monthly'),
-#}
+# sitemaps = {
+    # 'flatpages': FlatPageSitemap,
+    # 'blog': GenericSitemap(blog_dict, priority=0.4, changefreq='monthly'),
+    # 'photos': GenericSitemap(photo_dict, priority=0.7, changefreq='weekly'),
+    # 'videos': GenericSitemap(video_dict, priority=0.7, changefreq='weekly'),
+    # 'tours': GenericSitemap(tour_dict, priority=0.8, changefreq='monthly'),
+    # 'taxa': GenericSitemap(taxon_dict, priority=1.0, changefreq='weekly'),
+    # 'tags': GenericSitemap(tag_dict, priority=0.8, changefreq='weekly'),
+    # 'places': GenericSitemap(place_dict, priority=0.8, changefreq='weekly'),
+    # 'cities': GenericSitemap(city_dict, priority=0.6, changefreq='monthly'),
+    # 'states': GenericSitemap(state_dict, priority=0.4, changefreq='monthly'),
+    # 'countries': GenericSitemap(country_dict, priority=0.4, changefreq='monthly'),
+    # 'authors': GenericSitemap(author_dict, priority=0.8, changefreq='weekly'),
+    # 'sources': GenericSitemap(source_dict, priority=0.7, changefreq='weekly'),
+    # 'references': GenericSitemap(reference_dict, priority=0.5, changefreq='monthly'),
+# }
 
-
-def extra(model, field):
-    return {'model_name': model, 'field': field}
 
 urlpatterns = [
         path('admin/', admin.site.urls),
@@ -64,7 +56,7 @@ urlpatterns = [
         #url(r'^i18n/', include('django.conf.urls.i18n')),
 
         # Sitemaps
-        #(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+        #url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
 
         # Feeds
         # (r'^feed/latest/$', LatestMedia()),
@@ -73,14 +65,5 @@ urlpatterns = [
         # (r'^(?P<field>[^\d]+)/(?P<slug>[\w\-]+)/feed/(?P<type>[^\d]+)/$', MetaMedia()),
         # (r'^tours/feed/$', LatestTours()),
 
-        # Manage
-        #(r'^private/$', hidden_page),
-        #(r'^fixmedia/$', fixmedia_page),
-
-        # XXX Padronizar syntax de passar argumentos para views?
-
-        # AJAX Search suggestions
-        #(r'^ajax_search/', ajax_autocomplete),
-
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+        # Site media
+        ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cifonauta/wsgi.py
+++ cifonauta/wsgi.py
@@ -4,11 +4,13 @@ WSGI config for cifonauta project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cifonauta.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cifonauta.settings")
+
 application = get_wsgi_application()

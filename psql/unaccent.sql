2338e4f1d9c2d8a460efd870020276358bc3911c
Bruno Vellutini <organelas@gmail.com>
2023-05-10 10:59:15 +0200

    Remove deprecated psql commands

--- psql/unaccent.sql
+++
@@ -1,3 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS unaccent;
-CREATE TEXT SEARCH CONFIGURATION portuguese_unaccent( COPY = portuguese );
-ALTER TEXT SEARCH CONFIGURATION portuguese_unaccent ALTER MAPPING FOR hword, hword_part, word WITH unaccent, portuguese_stem;

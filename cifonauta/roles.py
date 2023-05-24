612c4918bf2a60880512abe7508b13a802d64042
Storti-Scatambuli <joao.storti@injunior.com.br>
2023-05-23 21:44:51 -0300

    feat: Specialist user created

--- cifonauta/roles.py
+++ cifonauta/roles.py
@@ -4,3 +4,8 @@ class Author(AbstractUserRole):
     available_permissions = {
         'upload_media': True
     }
+
+class Specialist(AbstractUserRole):
+    available_permissions = {
+        'metadata_edit_content': True
+    }

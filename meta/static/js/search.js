70eaf0e8928ea6f3639b19570b81b0a5c4de2b47
felyppe nunes <luiz.felyppe@injunior.com.br>
2023-06-09 12:02:16 -0300

    fix: small bug with the bottom input

--- meta/static/js/search.js
+++ meta/static/js/search.js
@@ -1,20 +1,28 @@
 function showCheckboxOptions(htmlTag, event) {
     event.preventDefault()
-    let checkboxOptionsDiv = htmlTag.nextElementSibling
-    let dropdownArrow = htmlTag.children[1]
-    let displayBottom = document.querySelector('#display-bottom')
-    let divDisplayValue = window.getComputedStyle(checkboxOptionsDiv).display
-    console.log(divDisplayValue)
+
+    const dropdownArrow = htmlTag.children[1]
+    const checkboxOptionsDiv = htmlTag.nextElementSibling
+    const divDisplayValue = window.getComputedStyle(checkboxOptionsDiv).display
     if (divDisplayValue == 'none') {
         checkboxOptionsDiv.style.display = 'block'
         dropdownArrow.style.rotate = '90deg'
-        displayBottom.style.display = 'block'
     }
     else {
         checkboxOptionsDiv.style.display = 'none'
         dropdownArrow.style.rotate = '0deg'
-        displayBottom.style.display = 'none'
     }
-
     
+    const moreFiltersButton = document.querySelector(".more-filters-button")
+    if (htmlTag.contains(moreFiltersButton)) {
+        const displayBottom = document.querySelector('#display-bottom')
+        const moreFilters = document.querySelector(".more-filters")
+        const moreFiltersDisplay = window.getComputedStyle(moreFilters).display
+        if (moreFiltersDisplay == 'none') {
+            displayBottom.style.display = 'none'
+        }
+        else {
+            displayBottom.style.display = 'block'
+        }
+    }  
 }

4c390976aa22b8b37da4a89d8f94715a19a49f6d
Storti-Scatambuli <joao.storti@injunior.com.br>
2023-06-29 19:58:37 -0300

    feat: created user creation form

--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .forms import UserCifonautaCreationForm
+from django.contrib import messages
 # Create your views here.
+
+def user_creation(request):
+    form = UserCifonautaCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+    return render(request, 'users/user_creation.html', {'form': form})

8af3edb58af0c39066ac6e8dfb5b70d1f8da72a0
Storti-Scatambuli <joao.storti@injunior.com.br>
2023-07-19 15:31:39 -0300

    fix: merge conflicts fixed

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('create/', views.user_creation, name='Create User'),
     path('record/', views.user_creation, name='Create User'),
     path('login/', views.login_view, name='Login'),
-    #path('captcha/', include('captcha.urls')),
+    path('captcha/', include('captcha.urls')),
     path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
     path('reset/confirm/<str:uidb64>/<str:token>/',views.PasswordResetConfirmView.as_view(), name='password_reset_confirm_custom')
 ]

6e2dc8ac0066652c4a2ed231b453471b2a274f87
felyppe nunes <luiz.felyppe@injunior.com.br>
2023-08-01 13:30:17 -0300

    fix: for signals to work

--- meta/apps.py
+++ meta/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class MetaConfig(AppConfig):
+    default_app_config = 'meta.MetaConfig'
     name = 'meta'
+
+    def ready(self):
+        import meta.signals

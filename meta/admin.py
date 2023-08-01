44896b9941916b4689a920793ca3ebf8d4089b8e
felyppe nunes <luiz.felyppe@injunior.com.br>
2023-08-01 13:29:27 -0300

    refactor: show media author in Django admin

--- meta/admin.py
+++ meta/admin.py
@@ -18,7 +18,7 @@ class FlatPageAdmin(TranslationAdmin):
 
 
 class MediaAdmin(TranslationAdmin):
-    list_display = ('title', 'filepath', 'is_public', 'highlight', 'title', 'caption', 'timestamp')
+    list_display = ('title', 'filepath', 'is_public', 'highlight', 'title', 'caption', 'timestamp', 'author')
     list_filter = ('is_public', 'highlight', 'timestamp', 'person', 'tag', 'taxon')
 
 

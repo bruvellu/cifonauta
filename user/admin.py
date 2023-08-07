da56d1a6291b1703547b3091adbd1941a12d9820
felyppe nunes <luiz.felyppe@injunior.com.br>
2023-08-07 14:53:33 -0300

    feat: actions to make/remove author

--- user/admin.py
+++ user/admin.py
@@ -4,26 +4,39 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import UserCifonautaCreationForm
 from .models import UserCifonauta
 
+@admin.action(description="Tornar autor")
+def make_author_action(modeladmin, request, queryset):
+    for pessoa in queryset:
+        pessoa.is_author = True
+        pessoa.save()
+
+@admin.action(description="Remover autor")
+def remove_author_action(modeladmin, request, queryset):
+    for pessoa in queryset:
+        pessoa.is_author = False
+        pessoa.save()
+
 class UserCifonautaAdmin(UserAdmin):
     add_form = UserCifonautaCreationForm
     model = UserCifonauta
-    list_display = ("username", "email", "is_staff", "is_active", 'orcid', "first_name", "last_name", "idlattes")
-    list_filter = ("email", "curadoria", "is_staff", "is_active", "username", 'orcid', "first_name", "last_name", "idlattes")
+    list_display = ("username", "email", "is_author", "is_staff", "is_active", 'orcid', "first_name", "last_name", "idlattes")
+    list_filter = ("email", "curadoria", "is_author", "is_staff", "is_active", "username", 'orcid', "first_name", "last_name", "idlattes")
     fieldsets = (
         (None, {"fields": ("email", "password", "username", 'orcid', "first_name", "last_name", "idlattes")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "curadoria","groups", "user_permissions")}),
+        ("Permissions", {"fields": ("is_author", "is_staff", "is_active", "curadoria")}),
     )
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
             "fields": (
                 "email", "password1", "password2", "is_staff",
-                "is_active", "groups", "user_permissions", "username", 'orcid', "first_name", "last_name", "idlattes"
+                "is_active", "username", 'orcid', "first_name", "last_name", "idlattes"
             )}
         ),
     )
     search_fields = ("email",)
     ordering = ("email",)
+    actions = [make_author_action, remove_author_action]
 
 
 admin.site.register(UserCifonauta, UserCifonautaAdmin)

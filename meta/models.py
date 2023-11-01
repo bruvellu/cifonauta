db7c431710e9b9d1d4b71e63f352d694a0cb4db1
Bruno Vellutini <organelas@gmail.com>
2023-11-01 16:23:08 +0100

    Add specialists field as another Person m2m

--- meta/models.py
+++ meta/models.py
@@ -111,7 +111,9 @@ class Media(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, verbose_name=_('usuário do arquivo'),
                              help_text=_('Usuário que fez o upload do arquivo.'), related_name='user')
     authors = models.ManyToManyField('Person', blank=True, verbose_name=_('autores do arquivo'),
-                                     help_text=_('Autores associates a este arquivo.'), related_name='authors')
+                                     help_text=_('Autores associados a este arquivo.'), related_name='authors')
+    specialists = models.ManyToManyField('Person', blank=True, verbose_name=_('especialistas associados'),
+                                     help_text=_('Especialistas associados a este arquivo.'), related_name='specialists')
 
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,
                                verbose_name=_('autor'), help_text=_('Autor da mídia.'), related_name='author')

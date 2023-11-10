fa034372abd6cddede7c2d0acdd3b8c3b7fd5317
felyppe nunes <luiz.felyppe@injunior.com.br>
2023-11-10 17:51:52 -0300

    refactor: improved small detail in checkboxes

--- meta/static/js/adm_select_all_records.js
+++ meta/static/js/adm_select_all_records.js
@@ -1,12 +1,23 @@
 const selectAllRecords = document.querySelector('#select-all-records')
-const selectedRecordList = document.querySelectorAll('.selected-record')
+const selectedRecordAll = document.querySelectorAll('.selected-record')
+let selectedRecordArray = Array.from(selectedRecordAll)
 
-selectAllRecords.addEventListener('click', () => {
-  selectedRecordList.forEach(record => {
-    if (selectAllRecords.checked) {
-      record.checked = true
+selectedRecordAll.forEach(record => {
+  record.addEventListener('click', () => {
+    const AllRecordsSelected = selectedRecordArray.every(record => record.checked)
+
+    if (AllRecordsSelected) {
+      selectAllRecords.checked = true
     } else {
-      record.checked = false
+      selectAllRecords.checked = false
     }
   })
+})
+
+selectAllRecords.addEventListener('click', () => {
+  if (selectAllRecords.checked) {
+    selectedRecordAll.forEach(record => record.checked = true)
+  } else {
+    selectedRecordAll.forEach(record => record.checked = false)
+  }
 })

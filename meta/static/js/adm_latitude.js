617c398b5281aa5e538c3d4660ac6418ee05b4ee
INJRVitoriaOliveira <vitoria.oliveira@injunior.com.br>
2024-01-18 23:33:24 -0300

    feat: google maps pop-up added

---
+++ meta/static/js/adm_latitude.js
@@ -0,0 +1,45 @@
+const referenceModal = new Modal({
+    modalContent: document.querySelector('[data-modal="latitude"]'),
+    modalTrigger: document.querySelector('[data-open-modal="latitude"]'),
+    modalClose: document.querySelector('[data-close-modal="latitude"]')
+})
+function myMap() {
+
+window.lat = document.getElementById("id_latitude").value
+window.lng = document.getElementById("id_longitude").value
+
+if (window.lat == '' || window.lng == ''){
+    lat = -23.828094
+    lng = -45.421718
+}
+var latLngInitial = new google.maps.LatLng(lat,lng)
+var mapProp= {
+    center:latLngInitial,
+    zoom:10,
+    gestureHandling: "greedy",
+    streetViewControl: false,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+window.marker = new google.maps.Marker({
+    position: latLngInitial,
+    map: map,
+    });
+
+map.addListener("click", (e) => {
+    placeMarkerAndPanTo(e.latLng, map);
+    });
+}
+
+function placeMarkerAndPanTo(latLng, map) {
+    marker.setMap(null);
+    marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    });
+    document.getElementById("id_latitude").value = latLng.lat();
+    document.getElementById("id_longitude").value = latLng.lng();
+
+    map.panTo(latLng);
+    referenceModal.closeModal()
+}
+    

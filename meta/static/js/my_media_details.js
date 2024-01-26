ba752ce55de68bd8a9ffff6dbc7b04a681e1cce3
Felyppe <felyppe.nunes1@gmail.com>
2024-01-26 12:27:10 -0300

    fix: media changes modal

--- meta/static/js/my_media_details.js
+++ meta/static/js/my_media_details.js
@@ -1,9 +1,12 @@
 (() => {
-    new Modal({
-        modalContent: document.querySelector('[data-modal="media-changes"]'),
-        modalTrigger: document.querySelector('[data-open-modal="media-changes"]'),
-        modalClose: document.querySelector('[data-close-modal="media-changes"]')
-    })
+    const mediaChangesModal = document.querySelector('[data-modal="media-changes"]')
+    if (mediaChangesModal) {
+        new Modal({
+            modalContent: mediaChangesModal,
+            modalTrigger: document.querySelector('[data-open-modal="media-changes"]'),
+            modalClose: document.querySelector('[data-close-modal="media-changes"]')
+        })
+    }
 
     new CreateEntry({ fieldName: 'taxa' })
     new CreateEntry({ fieldName: 'location' })

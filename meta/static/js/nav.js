9fbd5d9c0f83e1c26e81ecc951c0d8469809d177
Felyppe <felyppe.nunes1@gmail.com>
2024-01-27 14:05:59 -0300

    feat: menu hamburger css of public website

--- meta/static/js/nav.js
+++ meta/static/js/nav.js
@@ -1,31 +1,54 @@
-function openMenu() {
-    let hamburgerMenuContainer = document.querySelector("#hamburger-menu-container")
-    hamburgerMenuContainer.classList.add('show-hamburger-menu')
-    isMenuOpened = true
-}
+(() => {
+    function openMenu() {
+        document.body.append(overlay)
 
-function closeMenu() {
-    let hamburgerMenuContainer = document.querySelector("#hamburger-menu-container")
-    hamburgerMenuContainer.classList.remove('show-hamburger-menu')
-}
+        hamburgerMenu.setAttribute('data-state', 'open')
+        closeNav.focus()
+    }
 
-document.addEventListener('click', (event)=>{
-    const hamburgerMenuContainer = document.querySelector('#hamburger-menu-container')
-    const hamburgerButton = document.querySelector('#hamburger-button')
+    function closeMenu() {
+        overlay.remove()
 
-    if (!hamburgerMenuContainer.contains(event.target) && 
-        !hamburgerButton.contains(event.target) &&
-        isMenuOpened) {
+        hamburgerMenu.setAttribute('data-state', 'close')
+        hamburger.focus()
+    }
+
+    function trapFocus(event) {
+        const firstFocusable = focusableElements[0]
+        const lastFocusable = focusableElements[focusableElements.length - 1]
+
+        if (event.key === 'Tab') {
+            if (event.shiftKey && document.activeElement === firstFocusable) {
+                event.preventDefault()
+                lastFocusable.focus()
+            } else if (!event.shiftKey && document.activeElement === lastFocusable) {
+                event.preventDefault()
+                firstFocusable.focus()
+            }
+        }
+    }
+
+    const hamburger = document.querySelector('[data-hamburger]')
+    const hamburgerMenu = document.querySelector('[data-hamburger-menu]')
+    const closeNav = hamburgerMenu.querySelector('[data-hamburger-menu-close]')
+
+    const overlay = document.createElement('div')
+    overlay.classList.add('overlay')
+
+    const focusableElements = [...hamburgerMenu.querySelectorAll(
+        'a, button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])')
+    ].filter(elem => {
+        if(!elem.hasAttribute('disabled') && !elem.hasAttribute('hidden') && elem.type != 'hidden') return elem
+    })
+
+    hamburger.addEventListener('click', openMenu)
+    closeNav.addEventListener('click', closeMenu)
+    hamburgerMenu.addEventListener('keydown', trapFocus)
+    overlay.addEventListener('click', closeMenu)
+
+    window.addEventListener('resize', ()=>{
+        if (window.innerWidth > 590) {
             closeMenu()
-    }
-})
-
-window.addEventListener('resize', ()=>{
-    if (window.innerWidth > 590) {
-        closeMenu()
-        /* let hamburgerMenuContainer = document.querySelector('hamburger-menu-container') */
-    }
-})
-
-
-let isMenuOpened = false
+        }
+    })
+})()

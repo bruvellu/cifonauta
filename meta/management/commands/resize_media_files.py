271110b55f9677574b8f6e2dedbea760f6cf28d7
Bruno Vellutini <organelas@gmail.com>
2024-11-20 10:32:16 +0100

    Improve options printing and argument names

--- meta/management/commands/resize_media_files.py
+++ meta/management/commands/resize_media_files.py
@@ -28,10 +28,10 @@ class Command(BaseCommand):
 
         parser.add_argument('--minutes', type=int, default=0,
                             help='Skip entries updated less than X minutes ago (default=0).')
-        parser.add_argument('--only-photo', action='store_true', dest='only_photo',
+        parser.add_argument('--only-photos', action='store_true', dest='only_photos',
                 help='Only update photos.')
 
-        parser.add_argument('--only-video', action='store_true', dest='only_video',
+        parser.add_argument('--only-videos', action='store_true', dest='only_videos',
                 help='Only update videos.')
 
         parser.add_argument('--skip-recent', action='store_true', dest='skip_recent',
@@ -39,31 +39,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        # Print options
-        print()
-        for k, v in options.items():
-            print(f'  {k}: {v}')
-        print()
-
         # Parse options
         id = options['id']
         number = options['number']
         days = options['days']
         hours = options['hours']
         minutes = options['minutes']
-        only_photo = options['only_photo']
-        only_video = options['only_video']
+        only_photos = options['only_photos']
+        only_videos = options['only_videos']
         skip_recent = options['skip_recent']
 
         # Start with all media files
         media = Media.objects.all()
 
         # Only photos
-        if only_photo:
+        if only_photos:
             media = media.filter(datatype='photo')
 
         # Only videos
-        if only_video:
+        if only_videos:
             media = media.filter(datatype='video')
 
         # Ignore recently updated files
@@ -71,8 +65,7 @@ class Command(BaseCommand):
             datelimit = timezone.now() - timezone.timedelta(
                     days=days,
                     hours=hours,
-                    minutes=minutes
-                    )
+                    minutes=minutes)
             media = media.filter(date_modified__lt=datelimit)
 
         # Limit the total number of taxa
@@ -81,10 +74,22 @@ class Command(BaseCommand):
         # If ID, ignore above and force processing
         if id:
             media = Media.objects.filter(id=id)
-            print(f'Processing single file ID={id} (ignoring other filters).')
+            print(f'\nProcessing single file ID={id} (ignoring other filters).')
         else:
-            print(f'Processing {number} files...')
-            print(f'photo={only_photo}, video={only_video}, recent={skip_recent}')
+            print(f'\nProcessing {number} files...')
+
+        # Print options
+        print()
+        for k, v in options.items():
+            ignore = ['settings', 'verbosity', 'pythonpath', 'traceback',
+                      'no_color', 'force_color', 'skip_checks', 'number']
+            if not id:
+                ignore.append('id')
+            if not skip_recent:
+                ignore.extend(['days', 'hours', 'minutes'])
+            if not k in ignore:
+                print(f'  {k}: {v}')
+        print()
 
         # Loop over taxon queryset, closing files when done
         for instance in media:

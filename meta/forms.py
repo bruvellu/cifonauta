d1fa948ceb261797fe388c8d1afc5e59028c4670
Bruno Vellutini <organelas@gmail.com>
2024-12-08 18:50:45 +0100

    Fix non-appearing taxa w/o media on search form

--- meta/forms.py
+++ meta/forms.py
@@ -887,10 +887,11 @@ class DisplayForm(forms.Form):
 
     # TODO: Excluding taxon without media does not take into account descendants
     # For example, chordata has no media, but its descendants have. It should not be excluded here.
+    # But there is a performance cost that needs to be dealt with.
     taxon = forms.ModelMultipleChoiceField(
         required=False,
-        #queryset=Taxon.objects.all()
-        queryset=Taxon.objects.exclude(media__isnull=True)
+        #queryset=Taxon.objects.exclude(media__isnull=True)
+        queryset=Taxon.objects.all()
         .exclude(media__status__in=["loaded", "draft", "submitted"])
         .order_by("name"),
         widget=forms.SelectMultiple(

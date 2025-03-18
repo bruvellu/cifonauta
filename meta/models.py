96cdf36dc323bbbb693a59e622b3367c62b0caa3
Bruno Vellutini <organelas@gmail.com>
2025-03-18 21:57:21 +0100

    Make Taxon slug field unique=True

--- meta/models.py
+++ meta/models.py
@@ -876,7 +876,7 @@ class Category(models.Model):
 class Taxon(MPTTModel):
     name = models.CharField(_('nome'), max_length=256,
             help_text=_('Nome do táxon.'))
-    slug = models.SlugField(_('slug'), max_length=256, blank=True,
+    slug = models.SlugField(_('slug'), max_length=256, blank=True, unique=True,
             help_text=_('Slug do nome do táxon.'))
     rank = models.CharField(_('rank'), max_length=256, blank=True,
             help_text=_('Ranking taxonômico do táxon.'))
